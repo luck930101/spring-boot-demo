7bb6dc3efff2f7b0f889d712b38a207d8a7de7bd
Yubo Zhang <clannad93@gmail.com>
2019-02-16 16:26:32 -0800

    Initialize Form

--- src/main/java/com/yuboz/springbootdemo/web/BookController.java
+++ src/main/java/com/yuboz/springbootdemo/web/BookController.java
@@ -7,7 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
+/**
+ * Get Book List
+ */
 @Controller
 public class BookController {
 
@@ -15,10 +22,15 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/books")
-    public String list(){
+    public String list( Model model){
+        List<Book> books = bookService.findall();
+        model.addAttribute("books",books);
         return "books";
     }
 
+    /**
+     * Get Book Detail
+     */
 
     @GetMapping("/books/{id}")
     public String detail(@PathVariable long id, Model model){
@@ -30,5 +42,49 @@ public class BookController {
 
         model.addAttribute("book",book);
         return "book";
-    };
+    }
+
+    /**
+     * redirect to submit page
+     * @return
+     */
+    @GetMapping("/books/addbook")
+    public String AddBookPage(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    /**
+     * book List page
+     * @return
+     */
+    @PostMapping("/books")
+    public String post(Book book, final RedirectAttributes attributes){
+        Book savedbook = bookService.save(book);
+        if (savedbook !=null){
+            attributes.addFlashAttribute("message","<"+savedbook.getName()+"> saved!");
+        }
+
+
+        return "redirect:/books";
+        /** POST ----->redirect ---->GET
+         * Two Requests, can not pass just one Model
+         *
+         * should use Flash Attribute
+         */
+    }
+
+
+    /**
+     * redirect to update page
+     * @return
+     */
+
+    @GetMapping("/books/{id}/addbook")
+    public String editPage(@PathVariable long id,Model model){
+        Book book = bookService.findOne(id);
+        model.addAttribute("book",book);
+
+        return "addbook";
+    }
 }
